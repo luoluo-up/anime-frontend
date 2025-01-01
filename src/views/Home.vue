d061754a668ddf12042196c2b5d67e5441bceb5f
ohsehun <601119280@qq.com>
2025-01-02 03:37:38 +0800

    修复修复

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,39 +15,51 @@
 import NavBar from '../components/nav-bar.vue';
 import Swiper from '../components/swiper.vue';
 import SmallTopic from '../components/small-topic.vue';
-import { ref } from 'vue'
+import { ref } from 'vue';
+
+// 引入图片  
+import img1 from '../assets/images/home/1.jpg';
+import img2 from '../assets/images/home/2.jpg';
+import img3 from '../assets/images/home/3.jpg';
+import img4 from '../assets/images/home/4.jpg';
+import img5 from '../assets/images/home/5.jpg';
+import img6 from '../assets/images/home/6.jpg';
+import img7 from '../assets/images/home/7.jpg';
+import img8 from '../assets/images/home/8.jpg';
+
+// 账户信息  
 const info = ref([{
     title: '约会大作战VI',
     tips: '2.9',
-    imgUrl: require('../assets/images/home/1.jpg')
+    imgUrl: img1
 }, {
     title: '约会大作战V',
     tips: '1.9',
-    imgUrl: require('../assets/images/home/2.jpg')
+    imgUrl: img2
 }, {
     title: '约会大作战IV',
     tips: '2.19',
-    imgUrl: require('../assets/images/home/3.jpg')
+    imgUrl: img3
 }, {
     title: '约会大作战III',
     tips: '3.21',
-    imgUrl: require('../assets/images/home/4.jpg')
+    imgUrl: img4
 }, {
     title: '约会大作战II',
     tips: '6.1',
-    imgUrl: require('../assets/images/home/5.jpg')
+    imgUrl: img5
 }, {
     title: '约会大作战',
     tips: '1.1',
-    imgUrl: require('../assets/images/home/6.jpg')
+    imgUrl: img6
 }, {
     title: '约会大作战OVA',
     tips: '3.1',
-    imgUrl: require('../assets/images/home/7.jpg')
+    imgUrl: img7
 }, {
     title: '约会大作战外传',
     tips: '3.1',
-    imgUrl: require('../assets/images/home/8.jpg')
+    imgUrl: img8
 }]);
 
 // 随机打乱数组并选择前四项  
@@ -59,7 +71,7 @@ const shuffleAndSelect = (array) => {
 
 const info1 = ref(shuffleAndSelect(info.value)); // 生成 info1  
 // 生成可用图片数组  
-const availableImages = Array.from({ length: 10 }, (_, i) => `../src/assets/images/home/${i + 1}.jpg`);
+const availableImages = Array.from({ length: 10 }, (_, i) => require(`../assets/images/home/${i + 1}.jpg`));
 
 // 生成 info2，包含 10 项内容，每项使用所有图片  
 const generateInfo2 = () => {
@@ -76,7 +88,6 @@ const generateInfo2 = () => {
 
 // 生成 info2  
 const info2 = ref(generateInfo2());
-
 
 // 生成 info3，包含 15 项内容，随机选择图片  
 const generateInfo3 = () => {
